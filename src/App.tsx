488f74059fd583dbb105b0a87b3274774c893bb2
Reyhan Kaplan <me@reyhan.dev>
2024-07-29 13:51:57 +0300

    feat: update name

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import './App.css'
 
 export const App = (): ReactElement => (
   <main>
-    <h1>Reyhan Ünal</h1>
+    <h1>Reyhan Kaplan</h1>
     <p>Frontend Developer</p>
     <a href="mailto:me@reyhan.dev" className="email-box">
       me@reyhan.dev
     </a>
     <ul>
       <li>
-        <a href="https://github.com/reyhankaplan" className="social-link">
+        <a
+          href="https://github.com/reyhankaplan"
+          target="_blank"
+          rel="noreferrer"
+          className="social-link"
+        >
           GitHub
         </a>
       </li>
       <li>
         <a
           href="https://www.linkedin.com/in/reyhankaplan"
+          target="_blank"
+          rel="noreferrer"
           className="social-link"
         >
           LinkedIn
